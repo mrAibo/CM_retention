94a2a71a767c94464b6eecda25819dd447be7985
mrAibo <93589172+mrAibo@users.noreply.github.com>
2026-08-26 12:33:48 +0200

    chore: bump version to 0.3.2

--- src/CmRetention.java
+++ src/CmRetention.java
@@ -1,7 +1,7 @@
 import com.ibm.mm.sdk.common.DKException;
 
 public final class CmRetention {
-    static final String VERSION = "0.3.1";
+    static final String VERSION = "0.3.2";
 
     private CmRetention() {}
 
