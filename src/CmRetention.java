25dbf108a432febec3962cafdc7d46c82f5e01bc
mrAibo <93589172+mrAibo@users.noreply.github.com>
2026-08-27 07:23:24 +0200

    chore: bump version to 0.4.0

--- src/CmRetention.java
+++ src/CmRetention.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 import java.util.Locale;
 
 public final class CmRetention {
-    static final String VERSION = "0.3.5";
+    static final String VERSION = "0.4.0";
 
     private CmRetention() {}
 
